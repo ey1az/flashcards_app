05e18dc1d73c51187fdfacafcc9115a7f817c177
ey1az <aisser.cool@gmail.com>
2023-12-13 12:15:15 +0400

    App Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./HomePage";
+import Flashcards from "./Flashcards";
+
+import "./CSS/App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home-page">Home Page</Link>
+            </li>
+            <li>
+              <Link to="/flashcards-page">Flashcards</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/assignment3_wm1" element={<HomePage />} />
+          <Route path="/home-page" element={<HomePage />} />
+          <Route path="/flashcards-page" element={<Flashcards />} />
+        </Routes>
+
+      </div>
+    </Router>
   );
 }
-
 export default App;
