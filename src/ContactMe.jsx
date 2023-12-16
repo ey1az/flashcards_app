4d13a583228d6482623edaf2e08301456240cdbf
ey1az <aisser.cool@gmail.com>
2023-12-16 20:10:19 +0400

    ContactMe Page Create, App.css Update

---
+++ src/ContactMe.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./CSS/ContactMe.css";
+
+function ContactMe() {
+    return (
+        <div>
+            <p>This is the ContactMe page.</p>
+        </div>
+    );
+}
+
+export default ContactMe;
