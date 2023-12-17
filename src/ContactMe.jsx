942e0a8236cf07534e81e02060bca8f1f1b0b2a5
ey1az <aisser.cool@gmail.com>
2023-12-18 01:17:27 +0400

    ContactMe Update

--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -3,8 +3,35 @@ import "./CSS/ContactMe.css";
 
 function ContactMe() {
     return (
-        <div>
-            <p>This is the ContactMe page.</p>
+        <div className="contact-me-cont">
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Subject:
+                    <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} required />
+                </label>
+                <label>
+                    Email:
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                </label>
+                <label>
+                    Message:
+                    <textarea value={content} onChange={(e) => setContent(e.target.value)} required />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+
+            <div className="messages">
+                <h2>Messages:</h2>
+                <ul>
+                    {messages.map((message) => (
+                        <li className="messages-list-comp" key={message.id}>
+                            <strong>Subject:</strong> {message.messageSubject} <br />
+                            <strong>Email:</strong> {message.messageEmail} <br />
+                            <strong>Content:</strong> {message.messageContent}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
