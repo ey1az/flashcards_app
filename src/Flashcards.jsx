9df7191e351f8b9ab715961aff009534e3eb0905
ey1az <aisser.cool@gmail.com>
2023-12-17 14:50:58 +0400

    Flashcards.jsx Update #1

--- src/Flashcards.jsx
+++ src/Flashcards.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./CSS/Flashcard.css";
 
-function Flashcards() {
-    return (
-        <div>
-            <p>Flashcards will be here.</p>
-        </div>
-    );
-}
+const FlashCards = ({ flashCard, onDelete, onEdit}) => {
+  const [turn, setTurn] = useState(false);
+  const [height, setHeight] = useState("initial");
+  const [status, setStatus] = useState(flashCard.questionStatus || "Want to Learn");
+  const [editMode, setEditMode] = useState(false);
+  const [editedQuestionTitle, setEditedQuestionTitle] = useState(flashCard.questionTitle);
+  const [editedQuestionAnswer, setEditedQuestionAnswer] = useState(flashCard.questionAnswer);
+  const [setMarkedAsLearned] = useState(flashCard.questionStatus === "Learned");
+  const [buttonText, setButtonText] = useState(status === "Want to Learn" ? "Mark as Noted" : "Mark as Learned");
 
-export default Flashcards;
+  const questionTitleEl = useRef();
+  const questionAnswerEl = useRef();
+  const questionStatusEl = useRef();
+
+  const handleClick = () => {
+    if (!editMode) {
+      setTurn(!turn);
+    }
+  };
+
+  const handleMouseEnter = () => {
+    setHeight("auto");
+  };
+
+  const handleMouseLeave = () => {
+    setHeight("initial");
+  };
+};
+
+export default FlashCards;
