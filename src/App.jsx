a2f379809829a662babebc4c7441a44e9f1d4888
ey1az <aisser.cool@gmail.com>
2023-12-18 21:52:03 +0400

    Major Changes: App.jsx, ContactMe Page, JSON

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import HomePage from "./HomePage";
-import Flashcards from "./Flashcards";
+import FlashcardList from "./FlashcardList";
+import ContactMe from "./ContactMe";
 
 import "./CSS/App.css";
 
-function App() {
+const App = () => {
+  const [flashCards, setFlashCards] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.PUBLIC_URL}/data/flashcardData.json`)
+      .then((response) => response.json())
+      .then((data) => {
+        const updatedFlashCards = data.flashCards.map((question) => {
+          return {
+            id: question.id,
+            questionTitle: question.questionTitle,
+            questionAnswer: question.questionAnswer,
+            questionOptions: question.questionOptions,
+            questionDate: question.questionDate,
+            questionStatus: question.questionStatus,
+          };
+        });
+        setFlashCards(updatedFlashCards);
+      })
+      .catch((error) => {
+        console.error("Error fetching flashcards:", error);
+      });
+  }, []);
+
   return (
     <Router>
       <div className="App">
-
         <nav>
           <ul>
             <li>
@@ -18,17 +41,24 @@ function App() {
             <li>
               <Link to="/flashcards-page">Flashcards</Link>
             </li>
+            <li>
+              <Link to="/contact-me-page">Contact Me</Link>
+            </li>
           </ul>
         </nav>
 
         <Routes>
           <Route path="/assignment3_wm1" element={<HomePage />} />
           <Route path="/home-page" element={<HomePage />} />
-          <Route path="/flashcards-page" element={<Flashcards />} />
+          <Route
+            path="/flashcards-page"
+            element={<FlashcardList flashCards={flashCards}/>}
+          />
+          <Route path="/contact-me-page" element={<ContactMe />} />
         </Routes>
-
       </div>
     </Router>
   );
-}
+};
+
 export default App;
--- src/ContactMe.jsx
+++ src/ContactMe.jsx
@@ -2,38 +2,90 @@ import React, { useState, useEffect } from "react";
 import "./CSS/ContactMe.css";
 
 function ContactMe() {
-    return (
-        <div className="contact-me-cont">
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Subject:
-                    <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} required />
-                </label>
-                <label>
-                    Email:
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                </label>
-                <label>
-                    Message:
-                    <textarea value={content} onChange={(e) => setContent(e.target.value)} required />
-                </label>
-                <button type="submit">Submit</button>
-            </form>
+  const [subject, setSubject] = useState("");
+  const [email, setEmail] = useState("");
+  const [content, setContent] = useState("");
+  const [messages, setMessages] = useState([]);
 
-            <div className="messages">
-                <h2>Messages:</h2>
-                <ul>
-                    {messages.map((message) => (
-                        <li className="messages-list-comp" key={message.id}>
-                            <strong>Subject:</strong> {message.messageSubject} <br />
-                            <strong>Email:</strong> {message.messageEmail} <br />
-                            <strong>Content:</strong> {message.messageContent}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  const fetchMessages = async () => {
+    try {
+      const response = await fetch("http://localhost:3001/messages");
+      if (!response.ok) {
+        throw new Error(`Failed to fetch messages. Server responded with ${response.status}`);
+      }
+      const data = await response.json();
+      setMessages(data);
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:3001/messages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          messageSubject: subject,
+          messageEmail: email,
+          messageContent: content,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to submit message. Server responded with ${response.status}`);
+      }
+
+      setSubject("");
+      setEmail("");
+      setContent("");
+
+      fetchMessages();
+    } catch (error) {
+      console.error("Error submitting message:", error);
+    }
+  };
+
+  return (
+    <div className="contact-me-cont">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Subject:
+          <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} required />
+        </label>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        </label>
+        <label>
+          Message:
+          <textarea value={content} onChange={(e) => setContent(e.target.value)} required />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+
+      <div className="messages">
+        <h2>Messages:</h2>
+        <ul>
+          {messages.map((message) => (
+            <li className="messages-list-comp" key={message.id}>
+              <strong>Subject:</strong> {message.messageSubject} <br />
+              <strong>Email:</strong> {message.messageEmail} <br />
+              <strong>Content:</strong> {message.messageContent}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default ContactMe;
