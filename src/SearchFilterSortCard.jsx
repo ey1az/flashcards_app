56733de5e96b6ea44adcbc5111d2277f86aa0211
ey1az <aisser.cool@gmail.com>
2023-12-22 14:43:24 +0400

    SearchFilterSortCard Minor Changes

--- src/SearchFilterSortCard.jsx
+++ src/SearchFilterSortCard.jsx
@@ -12,4 +12,103 @@ const SearchFilterSortCard = ({ flashCards, onDelete, onEdit }) => {
   const [selectedCards, setSelectedCards] = useState(new Set());
   const cardsPerPage = 9;
 
+  const handleStatusChange = (e) => {
+    const status = e.target.value;
+    setSelectedStatus(status);
+  };
+
+  const handleSortChange = (e) => {
+    const sortOption = e.target.value;
+    setSelectedSortOption(sortOption);
+  };
+
+  const getSortingFunction = () => {
+    switch (selectedSortOption) {
+      case "ID":
+        return (a, b) => a.id - b.id;
+      case "Question":
+        return (a, b) => a.questionTitle.localeCompare(b.questionTitle);
+      case "Answer":
+        return (a, b) => a.questionAnswer.localeCompare(b.questionAnswer);
+      default:
+        return (a, b) => new Date(b.questionDate) - new Date(a.questionDate);
+    }
+  };
+
+  const handleCardSelect = (cardId) => {
+    setSelectedCards((prevSelected) => {
+      const newSelected = new Set(prevSelected);
+      if (newSelected.has(cardId)) {
+        newSelected.delete(cardId);
+      } else {
+        newSelected.add(cardId);
+      }
+      return newSelected;
+    });
+  };
+
+  const filteredFlashCards = visibleFlashcards
+    .filter((flashCard) => {
+      const searchString = searchText.toLowerCase();
+
+      return (
+        flashCard.questionTitle.toLowerCase().includes(searchString) ||
+        flashCard.questionOptions.some((option) => option.toLowerCase().includes(searchString)) ||
+        flashCard.questionAnswer.toLowerCase().includes(searchString)
+      );
+    })
+    .filter((flashCard) => {
+      if (selectedStatus === "all") {
+        return true;
+      } else {
+        return flashCard.questionStatus === selectedStatus;
+      }
+    })
+    .sort(getSortingFunction());
+
+  return (
+    <div>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search flashcards..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+      </div>
+      <div className="filter-opt">
+        <label className="filter-opt-select">
+          Filter by Status:
+          <select onChange={handleStatusChange}>
+            <option value="all">All</option>
+            <option value="Want to Learn">Want to Learn</option>
+            <option value="Noted">Noted</option>
+            <option value="Learned">Learned</option>
+          </select>
+        </label>
+        <label>
+          Sort by:
+          <select onChange={handleSortChange}>
+            <option value="default">Date</option>
+            <option value="ID">ID</option>
+            <option value="Question">Question</option>
+            <option value="Answer">Answer</option>
+          </select>
+        </label>
+      </div>
+      <div className="card-cont">
+        {filteredFlashCards.map((flashCard) => (
+          <div key={flashCard.id} className="flashcard-item">
+            <Flashcards
+              flashCard={flashCard}
+              onDelete={onDelete}
+              onEdit={onEdit}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
+
+export default SearchFilterSortCard;
